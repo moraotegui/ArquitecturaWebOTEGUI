e7d983934b2e92ae7de533f4fe4b12ee7ff254d5
moraotegui <moraotegui@gmail.com>
2025-05-14 16:21:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Server corriendo');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 
